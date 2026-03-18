d79257b2f051b883a37f0d133525f7e07fa03135
Szymon Zubek <me@szymonzubek.com>
2026-03-18 10:54:28 +0000

    Added simple project directory setup script
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Setting up basic project structure..."
+cd && cd .. && cd .. && cd workspaces && cd $(ls)
+uv init && uv venv && uv python install 3.14 && uv python update-shell
+mkdir -p src/${PWD##*/}
+mkdir docs
+mkdir data
+mkdir exports
+mv main.py src/${PWD##*/}/main.py
+
+echo "Cleaning home directory..."
+cd && rm -rf .bash_logout .bashrc .oh-my-zsh .zcom*
+
+echo "Installing LSPs, Linters, Formatters with Mason..."
+nvim --headless -c "MasonInstall gopls lua-language-server prettier ruff stylua tinymist ty typstyle"
+echo "Project setup finished."
