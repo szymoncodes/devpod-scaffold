18a50283e0d5b8f03e87d9358711ab41b08254bc
Szymon Zubek <me@szymonzubek.com>
2026-03-18 13:01:48 +0000

    Moved all language features to homebrew
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,12 +3,26 @@ echo "Setting up basic project structure..."
 
 echo "Initialising uv..."
 cd && cd .. && cd .. && cd workspaces && cd $(ls)
-uv init && uv venv && uv python install 3.14 && uv python update-shell
+uv init && uv venv 
 mkdir -p src/${PWD##*/}
 mkdir docs
 mkdir data
 mkdir exports
 mv main.py src/${PWD##*/}/main.py
+
+echo "Downloading extra pkgs..."
+brew install \
+  python \
+  golang \
+  npm \
+  gopls \
+  lua-language-server \
+  prettier \
+  ruff \
+  stylua \
+  tinymist \
+  ty \
+  typstyle
 
 echo "Cleaning home directory..."
 cd && rm -rf .bash_logout .bashrc .oh-my-zsh .zcom*
