4947cc8105530ba85577d3cdfda8d434e7722da8
Szymon Zubek <me@szymonzubek.com>
2026-03-18 21:22:35 +0000

    Fixed: Added Homebrew eval and moved package installing for loop before initialising uv project
    Signed-off-by: Szymon Zubek <me@szymonzubek.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,12 +2,18 @@
 echo "Setting up basic project structure..."
 
 echo "Installing project tools..."
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 packages=(
   python
   uv
   ruff
   ty
 )
+
+for pkg in "${packages[@]}"; do
+  echo "Installing $pkg..."
+  brew install $pkg
+done
 
 echo "Initialising uv..."
 cd .. && cd .. && cd workspaces && cd $(ls)
@@ -18,10 +24,5 @@ mkdir -p data
 mkdir -p exports
 mv main.py src/${PWD##*/}/main.py
 
-
-for pkg in "${packages[@]}"; do
-  echo "Installing $pkg..."
-  brew install $pkg
-done
 
 echo "Project setup finished."
